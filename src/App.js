b71f3dc57d92724db0cff957cfb1233a8c054a76
Sagi1500 <Sagi1500@gmail.com>
2022-04-13 08:57:38 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tryyyyyyyyyy <code>src/App.js</code> and save to reload.
+          TTTT <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
