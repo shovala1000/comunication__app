110721acf066919acdaa6bdaed25809078c28bed
Sagi1500 <Sagi1500@gmail.com>
2022-04-14 16:36:55 +0300

    update14.4 - userdata

---
+++ src/userData/myContacts.js
@@ -0,0 +1,30 @@
+export const myContacts =
+    [
+        {
+            imageURL: 'avatar2.png',
+            imageAlt: 'avatar',
+            contactName: 'Harry Kane',
+            latestMassage: 'last massage is...'
+        },
+        {
+            imageURL: 'avatar3.png',
+            imageAlt: 'avatar',
+            contactName: 'Lionel Messi',
+            latestMassage: 'last massage is...'
+        },
+        {
+            imageURL: 'avatar4.png',
+            imageAlt: 'avatar',
+            contactName: 'Christian Eriksen',
+            latestMassage: 'last massage is...'
+        },
+        {
+            imageURL: 'avatar5.png',
+            imageAlt: 'avatar',
+            contactName: 'Cristiano Ronaldo',
+            latestMassage: 'last massage is...'
+        },
+
+    ];
+
+export const selectedChat = {name: 'Cristian Eriksen'};
