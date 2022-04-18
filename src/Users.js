7a561a790f41ffcc2aa6e041f756e80c0aa14e0d
shoval <shovala1000@gmail.com>
2022-04-18 17:01:57 +0300

    sign up and sign in need to fix appearance on screen (and background) . start by <SignManagement/>

---
+++ src/Users.js
@@ -0,0 +1,16 @@
+
+function Users(){
+    // Register info
+    function Register(name, nickname, pass) {
+        this.username = name;
+        this.nickname = nickname;
+        this.password = pass;
+    }
+
+// User Login info
+    const database = [new Register("shoval", "shoval", "123")];
+
+
+
+};
+export default Users;
